c4dce519470274b9c5c2e1c8b36ef1e3955f6bfb
momothereal <momothereal.mc@gmail.com>
2017-09-22 10:58:22 -0400

    Use built-in sphinx microphone tool

--- src/main/java/ca/momoperes/guys/Main.java
+++ src/main/java/ca/momoperes/guys/Main.java
@@ -3,36 +3,29 @@ package ca.momoperes.guys;
 import edu.cmu.sphinx.api.SpeechResult;
 
 import java.io.IOException;
-import java.io.PipedInputStream;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BlockingQueue<SpeechResult> queue = new LinkedBlockingQueue<>();
-        PipedInputStream stream = new PipedInputStream();
-        new Thread(() -> {
-            try {
-                MicrophoneManager.openAndWriteTo(stream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start();
 
         VoiceRecognition recognition = new VoiceRecognition();
-        recognition.setStream(stream);
         recognition.start(queue::add);
 
         while (recognition.isRunning()) {
             try {
                 SpeechResult take = queue.take();
                 if (take != null) {
-                    System.out.println("Hypothesis: " + take.getHypothesis());
+                    String hypothesis = take.getHypothesis();
+                    String pronunciation = take.getResult().getBestPronunciationResult();
+                    System.out.println("Hypothesis: " + hypothesis);
+                    System.out.println("Pronunciation: " + pronunciation);
+
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        stream.close();
     }
 }
--- src/main/java/ca/momoperes/guys/VoiceRecognition.java
+++ src/main/java/ca/momoperes/guys/VoiceRecognition.java
@@ -2,11 +2,10 @@ package ca.momoperes.guys;
 
 
 import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import edu.cmu.sphinx.api.SpeechResult;
-import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class VoiceRecognition {
     private static final String PATH_ACOUSTIC_MODEL = "resource:/edu/cmu/sphinx/models/en-us/en-us";
@@ -14,11 +13,9 @@ public class VoiceRecognition {
     private static final String PATH_LANGUAGE_MODEL = "resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin";
 
     private final Configuration configuration;
-    private final StreamSpeechRecognizer recognizer;
-    private InputStream stream = null;
+    private final LiveSpeechRecognizer recognizer;
     private Thread localThread = null;
     private boolean running;
-    private WordRecognitionCallback callback;
 
     public VoiceRecognition() throws IOException {
         this(PATH_ACOUSTIC_MODEL, PATH_DICTIONARY, PATH_LANGUAGE_MODEL);
@@ -29,11 +26,7 @@ public class VoiceRecognition {
         configuration.setAcousticModelPath(acousticModel);
         configuration.setDictionaryPath(dictionary);
         configuration.setLanguageModelPath(languageModel);
-        this.recognizer = new StreamSpeechRecognizer(configuration);
-    }
-
-    public InputStream getStream() {
-        return stream;
+        this.recognizer = new LiveSpeechRecognizer(configuration);
     }
 
     public void stop() {
@@ -47,25 +40,16 @@ public class VoiceRecognition {
         running = false;
     }
 
-    public void setStream(InputStream stream) {
-        stop();
-        this.stream = stream;
-    }
-
     public void start(final WordRecognitionCallback callback) {
-        if (stream == null) {
-            throw new IllegalStateException("No stream set.");
-        }
         if (running) {
             throw new IllegalStateException("Word recognition is already active.");
         }
         if (localThread != null) {
             localThread.interrupt();
         }
-        this.callback = callback;
         this.running = true;
         localThread = new Thread(() -> {
-            recognizer.startRecognition(stream);
+            recognizer.startRecognition(true);
             SpeechResult result;
             while (running) {
                 result = recognizer.getResult();
